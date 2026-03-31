aa646bb17a5115397a4f9ac109271c90d0818b3a
carledholm-design <carl.edholm@gmail.com>
2026-03-31 10:42:41 -0400

    Remove module resolver from Babel config
    Removed module resolver configuration from Babel.

--- babel.config.js
+++ babel.config.js
@@ -4,13 +4,6 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
-      [
-        'module-resolver',
-        {
-          root: ['./src'],
-          alias: { '@': './src' },
-        },
-      ],
     ],
   };
 };
